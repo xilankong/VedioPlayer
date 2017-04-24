955dfdbbca48df0173ebe98a56f0d1d98a0b94e9
young.huang <young.huang@jfz.com>
2017-04-24 11:07:13 +0800

    Initial Commit

---
+++ VedioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VedioPlayer
+//
+//  Created by yanghuang on 2017/4/24.
+//  Copyright © 2017年 com.yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
